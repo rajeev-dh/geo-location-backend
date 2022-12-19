78d5326aa594823e484809d05ec15fa5a9241e91
rajeev-dh <rajeev@daffodilhealth.com>
2022-12-19 22:20:17 +0530

    fixed location

--- controllers/class.js
+++ controllers/class.js
@@ -82,28 +82,38 @@ const markAttendance = async (req, res) => {
       return res
         .status(400)
         .json({ error: true, message: "Student already marked Attendance" });
-    // const distance = Math.sqrt(
-    //   (+runningClass.location.latitude - +location.latitude) *
-    //     (+runningClass.location.latitude - +location.latitude) +
-    //     (+runningClass.location.longitude - +location.longitude) *
-    //       (+runningClass.location.longitude - +location.longitude)
-    // );
-    function distance(lat1, lon1, lat2, lon2) {
-      var p = 0.017453292519943295; // Math.PI / 180
-      var c = Math.cos;
-      var a =
-        0.5 -
-        c((lat2 - lat1) * p) / 2 +
-        (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
+    function distance(lat1, lat2, lon1, lon2) {
+      // The math module contains a function
+      // named toRadians which converts from
+      // degrees to radians.
+      lon1 = (lon1 * Math.PI) / 180;
+      lon2 = (lon2 * Math.PI) / 180;
+      lat1 = (lat1 * Math.PI) / 180;
+      lat2 = (lat2 * Math.PI) / 180;
 
-      return 12742 * 1000 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+      // Haversine formula
+      let dlon = lon2 - lon1;
+      let dlat = lat2 - lat1;
+      let a =
+        Math.pow(Math.sin(dlat / 2), 2) +
+        Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+
+      let c = 2 * Math.asin(Math.sqrt(a));
+
+      // Radius of earth in kilometers. Use 3956
+      // for miles
+      let r = 6371;
+
+      // calculate the result
+      return c * r;
     }
-    const distanc = distance(
-      runningClass.location.latitude,
-      runningClass.location.longitude,
-      location.latitude,
-      location.longitude
-    );
+    const distanc =
+      distance(
+        runningClass.location.latitude,
+        location.latitude,
+        runningClass.location.longitude,
+        location.longitude
+      ) * 1000;
     console.log(
       "distance: ",
       distanc,
